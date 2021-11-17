32fe239519cc19c51bfc54174fc2612338182d21
lakshmimurali <vmanikandan@zohocorp.com>
2021-11-17 11:14:59 +0530

    1 file modified

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,20 +1,43 @@
-import { screen, cleanup, render } from '@testing-library/react';
-import { screen, cleanup, render } from '@testing-library/react';
-import { screen, cleanup, render } from '@testing-library/react';
-import { screen, cleanup, render } from '@testing-library/react';
-import { screen, cleanup, render } from '@testing-library/react';
-import { screen, render, cleanup } from '@testing-library/react';
-import { cleanup, screen, render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import AddToDo from './App';
 
-test('Need to render the AddTo Component', () => {
+test('renders addto do text box', () => {
   render(<AddToDo />);
-  const toDoTextBox = screen.getByTestId('add-todo-box');
-  expect(toDoTextBox).toBeInTheDocument();
+  const toDoBox = screen.getByTestId('add-todo-box');
+  expect(toDoBox).toBeInTheDocument();
 });
 
-test('need to check to todo component rendered', () => {
+test('check placeholder exist for the textbox', () => {
   render(<AddToDo />);
-  const textBox = screen.getByTestId('add-to-box');
-  expect(textBox).toBeInTheDocument();
+  const toToDoBox = screen.getByPlaceholderText(
+    'It is time to plan your tasks'
+  );
+  expect(toToDoBox).toBeInTheDocument();
+});
+
+test('Check adding todo via button click works', () => {
+  render(<AddToDo />);
+  fireEvent.input(screen.getByTestId('add-todo-box'), {
+    target: { value: 'Learn React, Learn Unit Test cases' },
+  });
+  fireEvent.click(screen.getByTestId('SubmitToDo'), {
+    bubbles: true,
+    cancelable: true,
+  });
+  const todolisttext = screen.getByText('Learn React, Learn Unit Test cases');
+  expect(todolisttext).toBeInTheDocument();
+});
+
+test('Check adding todo via enter key in text box', () => {
+  render(<AddToDo />);
+  fireEvent.input(screen.getByTestId('add-todo-box'), {
+    target: { value: 'Learn Python, Learn Unit Test cases' },
+  });
+  fireEvent.keyDown(screen.getByTestId('add-todo-box'), {
+    key: 'Enter',
+    code: 'Enter',
+    charCode: 13,
+  });
+  const todolisttext = screen.getByText('Learn Python, Learn Unit Test cases');
+  expect(todolisttext).toBeInTheDocument();
 });
