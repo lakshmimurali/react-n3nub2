5aabda9ece0b548df6410a66dad29b9ba643981c
lakshmimurali <vmanikandan@zohocorp.com>
2021-11-18 14:37:20 +0530

    1 file modified

--- src/index.js
+++ src/index.js
@@ -3,9 +3,4 @@ import ReactDOM from 'react-dom';
 
 import AddToDo from './App';
 
-const hats = { title: 'Favorite Hats', contents: 'Fedoras are classy' };
-const footware = {
-  title: 'Favorite Footware',
-  contents: 'Flipflops are the best',
-};
 ReactDOM.render(<AddToDo />, document.getElementById('root'));
